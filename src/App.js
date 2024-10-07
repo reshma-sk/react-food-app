1c15a81ac26d87e7e542835f5e14dd3508eff801
Reshma <reshmashaik0910@gmail.com>
2024-10-07 11:00:01 +0100

    changed app src

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React,{lazy,Suspense} from 'react';
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
-import Login from './src/components/Login';
-import Header from "./src/components/Header";
-import Footer from "./src/components/Footer";
-import Contact from "./src/components/Contact";
-import Error from "./src/components/Error";
-import RestaurantMenu from "./src/components/RestaurantMenu";
-import { RestaurantShimmer } from './src/components/Shimmer';
-import "./index.css"
+import Login from './components/Login';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import RestaurantMenu from "./components/RestaurantMenu";
+import { RestaurantShimmer } from './components/Shimmer';
+import "../index.css"
 
-const About = lazy(() => import("./src/components/About"));
-const Body = lazy(() => import("./src/components/Body"));
+const About = lazy(() => import("./components/About"));
+const Body = lazy(() => import("./components/Body"));
 
 const App = ()=>{
     return(    
